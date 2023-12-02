b2612fe02743caed0b6930ffe48ac423fa124e24
Nicu Taban <nictbndeveloper@gmail.com>
2023-12-02 18:44:52 +0200

    Implements the list beers functionality

--- src/main/java/com/example/spring6restmvc/services/BeerService.java
+++ src/main/java/com/example/spring6restmvc/services/BeerService.java
@@ -2,8 +2,11 @@ package com.example.spring6restmvc.services;
 
 import com.example.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+    List<Beer> listBeers();
+
     Beer getBeerById(UUID id);
 }
