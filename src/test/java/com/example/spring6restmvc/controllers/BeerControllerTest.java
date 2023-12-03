3921cd8cd4caa95dd5425d6da1ea0a70aef1bcf9
Nicu Taban <nictbndeveloper@gmail.com>
2023-12-03 18:04:58 +0200

    Tests that we return a json response body

--- src/test/java/com/example/spring6restmvc/controllers/BeerControllerTest.java
+++ src/test/java/com/example/spring6restmvc/controllers/BeerControllerTest.java
@@ -1,6 +1,9 @@
 package com.example.spring6restmvc.controllers;
 
+import com.example.spring6restmvc.model.Beer;
 import com.example.spring6restmvc.services.BeerService;
+import com.example.spring6restmvc.services.BeerServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -8,7 +11,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.UUID;
@@ -22,11 +28,24 @@ class BeerControllerTest {
     @MockBean
     BeerService beerService;
 
+    // Only used to instantiate a beer entity
+    BeerServiceImpl beerServiceImpl;
+
+    @BeforeEach
+    void setup() {
+        beerServiceImpl = new BeerServiceImpl();
+    }
+
     @Test
     void getBeerById() throws Exception {
+        Beer testBeer = beerServiceImpl.listBeers().get(0);
+
+        given(beerService.getBeerById(any(UUID.class))).willReturn(testBeer);
+
         mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
 
     }
 }
