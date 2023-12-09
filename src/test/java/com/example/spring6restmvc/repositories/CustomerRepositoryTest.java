fce9afceaa6bfa3c7f2fc167cab1b46aef5357d8
Nicu Taban <nictbndeveloper@gmail.com>
2023-12-09 19:33:03 +0200

    Adds repository tests

---
+++ src/test/java/com/example/spring6restmvc/repositories/CustomerRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.example.spring6restmvc.repositories;
+
+import com.example.spring6restmvc.entities.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+class CustomerRepositoryTest {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testSaveCustomer() {
+        Customer savedCustomer = customerRepository.save(Customer.builder()
+                        .name("New Name")
+                .build());
+        assertThat(savedCustomer).isNotNull();
+        assertThat(savedCustomer.getId()).isNotNull();
+    }
+
+}
