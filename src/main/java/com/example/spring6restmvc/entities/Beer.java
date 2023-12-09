29fd61c72e5c4e4734a126c0d6b61be03049cab1
Nicu Taban <nictbndeveloper@gmail.com>
2023-12-09 19:21:54 +0200

    Sets up UUID generation

--- src/main/java/com/example/spring6restmvc/entities/Beer.java
+++ src/main/java/com/example/spring6restmvc/entities/Beer.java
@@ -1,10 +1,9 @@
 package com.example.spring6restmvc.entities;
 
 import com.example.spring6restmvc.model.BeerStyle;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Version;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -18,6 +17,9 @@ import java.util.UUID;
 @Entity
 public class Beer {
     @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
     @Version
     private Integer version;
