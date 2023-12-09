183bb3de107e66e5873844bbe4ecee7a222a1037
Nicu Taban <nictbndeveloper@gmail.com>
2023-12-09 19:17:22 +0200

    Creates JPA entities

---
+++ src/main/java/com/example/spring6restmvc/entities/Beer.java
@@ -0,0 +1,31 @@
+package com.example.spring6restmvc.entities;
+
+import com.example.spring6restmvc.model.BeerStyle;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Beer {
+    @Id
+    private UUID id;
+    @Version
+    private Integer version;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
