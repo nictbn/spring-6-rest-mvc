4ff32825cc86c90ce63e9e45e3058e8162246dd9
Nicu Taban <nictbndeveloper@gmail.com>
2023-12-09 22:19:25 +0200

    Adds integration tests between the beer controller and the beer service

---
+++ src/test/java/com/example/spring6restmvc/controllers/BeerControllerIT.java
@@ -0,0 +1,36 @@
+package com.example.spring6restmvc.controllers;
+
+import com.example.spring6restmvc.model.BeerDto;
+import com.example.spring6restmvc.repositories.BeerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+class BeerControllerIT {
+    @Autowired
+    BeerController beerController;
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<BeerDto> dtos = beerController.listBeers();
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList() {
+        beerRepository.deleteAll();
+        List<BeerDto> dtos = beerController.listBeers();
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+}
