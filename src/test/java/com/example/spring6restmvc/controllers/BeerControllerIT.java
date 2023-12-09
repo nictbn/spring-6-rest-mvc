66b2b33ade03c6134698faa930925b0b038756c2
Nicu Taban <nictbndeveloper@gmail.com>
2023-12-09 23:05:14 +0200

    Test get beer by id happy path and exception path

--- src/test/java/com/example/spring6restmvc/controllers/BeerControllerIT.java
+++ src/test/java/com/example/spring6restmvc/controllers/BeerControllerIT.java
@@ -1,5 +1,6 @@
 package com.example.spring6restmvc.controllers;
 
+import com.example.spring6restmvc.entities.Beer;
 import com.example.spring6restmvc.model.BeerDto;
 import com.example.spring6restmvc.repositories.BeerRepository;
 import org.junit.jupiter.api.Test;
@@ -9,8 +10,10 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class BeerControllerIT {
@@ -32,5 +35,19 @@ class BeerControllerIT {
         beerRepository.deleteAll();
         List<BeerDto> dtos = beerController.listBeers();
         assertThat(dtos.size()).isEqualTo(0);
+    }
+
+    @Test
+    void testGetById() {
+        Beer beer = beerRepository.findAll().get(0);
+        BeerDto dto = beerController.getBeerById(beer.getId());
+        assertThat(dto).isNotNull();
+    }
+
+    @Test
+    void testBeerIdNotFound() {
+        assertThrows(NotFoundException.class, () -> {
+            beerController.getBeerById(UUID.randomUUID());
+        });
     }
 }
