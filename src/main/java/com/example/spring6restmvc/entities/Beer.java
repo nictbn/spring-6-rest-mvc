594a234cb60a309950d43182d98d165e50667442
Nicu Taban <nictbndeveloper@gmail.com>
2023-12-12 01:42:39 +0200

    Adds constraint validation

--- src/main/java/com/example/spring6restmvc/entities/Beer.java
+++ src/main/java/com/example/spring6restmvc/entities/Beer.java
@@ -4,6 +4,7 @@ import com.example.spring6restmvc.model.BeerStyle;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
@@ -27,6 +28,8 @@ public class Beer {
     private Integer version;
     @NotNull
     @NotBlank
+    @Size(max = 50)
+    @Column(length = 50)
     private String beerName;
     @NotNull
     private BeerStyle beerStyle;
--- src/test/java/com/example/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/example/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -2,6 +2,7 @@ package com.example.spring6restmvc.repositories;
 
 import com.example.spring6restmvc.entities.Beer;
 import com.example.spring6restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 class BeerRepositoryTest {
@@ -26,6 +28,19 @@ class BeerRepositoryTest {
         beerRepository.flush();
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
+    }
+
+    @Test
+    void testSaveBeerNameTooLong() {
+        assertThrows(ConstraintViolationException.class, () -> {
+            Beer savedBeer = beerRepository.save(Beer.builder()
+                    .beerName("My Beer My Beer My Beer My Beer My Beer My Beer My Beer My Beer My Beer My Beer My Beer My Beer")
+                    .beerStyle(BeerStyle.PALE_ALE)
+                    .upc("12344")
+                    .price(new BigDecimal("11.22"))
+                    .build());
+            beerRepository.flush();
+        });
     }
 
 }
