ed89dc41b15bd896031b9c696d075541486941ae
Nicu Taban <nictbndeveloper@gmail.com>
2023-12-21 23:02:49 +0200

    Configures the resource server

--- src/main/java/com/example/spring6restmvc/configuration/SpringSecurityConfig.java
+++ src/main/java/com/example/spring6restmvc/configuration/SpringSecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.spring6restmvc.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,9 +10,9 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .anyRequest().authenticated().and()
-                .httpBasic(Customizer.withDefaults())
-                .csrf().ignoringRequestMatchers("/api/**");
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer().jwt();
         return http.build();
     }
 }
